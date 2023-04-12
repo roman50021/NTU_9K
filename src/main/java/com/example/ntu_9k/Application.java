c6d8314a1031ff7439b7dc19f1dba69c11366d20
“Roman <fedkoroma2@gmail.comgit config --global user.email fedkoroma2@gmail.com>
2023-04-12 15:08:24 +0300

    first commit

--- src/main/java/com/example/ntu_9k/Application.java
+++ src/main/java/com/example/ntu_9k/Application.java
@@ -10,8 +10,8 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
+        stage.setTitle("Сinema");
         stage.setScene(scene);
         stage.show();
     }

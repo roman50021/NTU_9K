636104557edbb8e3872414a9d997df91b7d538a2
“Roman <fedkoroma2@gmail.comgit config --global user.email fedkoroma2@gmail.com>
2023-04-20 18:02:51 +0300

    2.6 Функции вывода полностью работают!+ иконка

--- src/main/java/com/example/ntu_9k/Application.java
+++ src/main/java/com/example/ntu_9k/Application.java
@@ -2,6 +2,7 @@ package com.example.ntu_9k;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,10 @@ public class Application extends javafx.application.Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
         stage.setTitle("Сinema");
+
+        Image icon = new Image(getClass().getResourceAsStream("4910018.png"));
+        stage.getIcons().add(icon);
+
         stage.setScene(scene);
         stage.show();
 
